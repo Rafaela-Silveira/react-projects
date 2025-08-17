67bad0580a2986156ce1f75542d6702b84f01657
Rafaela-Silveira <u5179599092@gmail.com>
2025-08-17 19:15:21 +0100

    test

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavigationBar from './components/navBar/navBar'
+const App = () => {
+  return (
+    <div className='app'>
+      <NavigationBar></NavigationBar>
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/components/navBar/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './navBar.css'
+const navBar = () => {
+  return (
+    <div className='navBar'>
+      
+    </div>
+  )
+}
+
+export default navBar
